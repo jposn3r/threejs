c024727bf2a0ff0b37651c0c3a6dad40603e0130
Jake Posner <jposner91@gmail.com>
2024-08-29 12:19:21 -0400

    swap out master chief for spiderman, textures on master chief too big

--- scenes/MetaScene.js
+++ scenes/MetaScene.js
@@ -31,7 +31,7 @@ export default class MetaScene extends ParentScene {
             const promises = [];
 
             promises.push(this.loadGLTF(this.scene, '/ready-player-jake.glb', 'rp-jake', 1, { x: 0, y: 0, z: 0 }, false, 0, 0, 0));
-            promises.push(this.loadGLTF(this.scene, '/avatars/halo-infinite-master-chief-rigged-walk./scene.gltf', 'master-chief', .5, { x: 1.4, y: 0, z: 0 }, false, 0, 0, 0));
+            promises.push(this.loadGLTF(this.scene, '/avatars/rigged_spider-man_3d_model/scene.gltf', 'mando', 1, { x: 1.4, y: 0, z: 0 }, false, 0, 0, 0));
             promises.push(this.loadGLTF(this.scene, '/avatars/goku-rigged-animated/scene.gltf', 'goku', .6, { x: -1.4, y: 0, z: 0 }, false, 0, 0, 0));
 
             // Wait for all models to load
